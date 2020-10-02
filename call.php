dfd818286580d272989bfae0a32cd7b5d86d6018
Ahmadganss <41518810+Ahmadganss@users.noreply.github.com>
2020-10-02 07:51:55 +0700

    Update call.php

--- call.php
+++ call.php
@@ -1,5 +1,5 @@
 <?php
-// Limit 3kali Telpon Setiap Satu Nomor
+// Limit 3x Telepon Setiap Satu Nomor
 function send($phone){
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
